6229cf78e3aa91f10273b105a3a7998ca4913f6b
Kwitez0 <97664333+Kwitez0@users.noreply.github.com>
2022-01-21 11:31:52 +0100

    Update name.py

--- name.py
+++ name.py
@@ -15,3 +15,28 @@ Face to face now we escalatin when I have to put boots to asses
 Mean on ya like a dream when Im rumblin youre gonna scream mama
 So bring drama to the King Brahma then what?
 Comin at you with extreme mana''')
+
+def indiantechsupport():
+  print('Hello this is Amazon help center, my name is Jake how can I help you?')
+  #homepage
+  
+  print('Press 1 if you need help with an order you did not make' 
+        'Press 2 if you were scammed'
+        'Press 3 to exit')
+  #Informs which button does what
+  
+  choice = input('Enter here: ')
+  #Code that lets you write which alterative you wish for
+  
+  if choice == 1:
+    print('Please enter your credit card information and the 3 numbers behind: ')
+    #If 1 is chosen, this activates
+  
+  elif choice == 2:
+    print('lmfao ez u so bad L')
+     #If 2 is chosen, this activates
+  
+  elif choice == 3:
+    exit()
+     #If 3 is chosen, this activates
+    
