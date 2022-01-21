c753c797a25242bb438d4757bf2a9501be42b333
Kwitez0 <97664333+Kwitez0@users.noreply.github.com>
2022-01-21 10:40:02 +0100

    Update name.py

--- name.py
+++ name.py
@@ -1,1 +1,14 @@
 print("yo mama big")
+
+print('''its about drive its about power
+We stay hungry we devour
+Put in the work put in the hours and take whats ours ahoo
+Black and Samoan in my veins
+My culture bangin with Strange
+I change the game so whats my motherfuckin name Rock
+What they gonna get though?
+Desecration defamation if you wanna bring it to the masses
+Face to face now we escalatin when I have to put boots to asses
+Mean on ya like a dream when Im rumblin youre gonna scream mama
+So bring drama to the King Brahma then what?
+Comin at you with extreme mana''')
